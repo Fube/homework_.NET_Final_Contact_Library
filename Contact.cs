7be9cb2eb7fe82aa5b64cf2a79208e259dcce267
Fube <fubeitch@gmail.com>
2020-11-28 15:34:36 -0500

    Refactored Contact and CotnactManager from the main project into its own library

---
+++ Contact.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactLibrary
+{
+    public class Contact
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string PhoneNumber { get; }
+        public long? ID { get; }
+
+        public Contact(string fName, string lName, string phone=null)
+        {
+            ID = null;
+            FirstName = fName;
+            LastName = lName;
+            PhoneNumber = phone;
+        }
+
+        public Contact(long? id, string fName, string lName, string phone = null) : this(fName, lName, phone)
+        {
+            ID = id;
+        }
+
+
+
+        public override string ToString() => $"ID: {ID}, Name: {FirstName} {LastName}\nPhone Number: {PhoneNumber}";
+
+        public void Deconstruct(out long? id, out string fName, out string lName, out string phone)
+        {
+            fName = FirstName;
+            lName = LastName;
+            phone = PhoneNumber;
+            id = ID;
+        }
+    }
+}
---
+++ ContactManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ContactLibrary
+{
+    public sealed class ContactManager
+    {
+
+        private readonly List<Contact> _contacts;
+
+        private static readonly Lazy<ContactManager> LazyInstance = new Lazy<ContactManager>(() => new ContactManager());
+
+        public static ContactManager Instance => LazyInstance.Value;
+
+        private ContactManager()
+        {
+            _contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public void RemoveContact(long id) => _contacts.RemoveAll(n => n.ID == id);
+
+        public void ImportFromFile(Stream stream)
+        {
+            using (var parser = new TextFieldParser(stream))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    if (fields != null)
+                        AddContact(new Contact(fields[0], fields[2], fields.Length < 3 ? null : fields[3]));
+                }
+            }
+        }
+
+        public void ExportToFile(string path)
+        {
+            using (var sw = new StreamWriter(path))
+            {
+                foreach (var v in _contacts)
+                {
+                    sw.WriteLine($"{v.FirstName},{v.LastName},{ v.PhoneNumber ?? "null" }");
+                    sw.Flush();
+                }
+            }
+        }
+
+    }
+}
